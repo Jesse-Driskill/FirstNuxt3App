f8fedccb3742de29c80095e4a80ae147176bb8e4
Jesse Driskill <jesse@iMac-de-julienmaltais.local>
2023-03-26 22:10:37 -0400

    Adds listing create page

---
+++ cartrader/components/Toilet/Ad/Select.vue
@@ -0,0 +1,14 @@
+<script setup>
+    const props = defineProps({
+        title: String,
+        options: Array
+    });
+</script>
+<template>
+    <div class="flex flex-col w-[48%] mt-2">
+        <label for="" class="text-cyan-500 mb-1 text-sm">{{ title }}</label>
+        <select name="" id="" class="p-2 border w-100 rounded">
+            <option v-for="option in options" :key="option.id" :value="option">{{ option }}</option>
+        </select>
+    </div>
+</template>
---
+++ cartrader/pages/profile/listings/create.vue
@@ -0,0 +1,18 @@
+<script setup>
+    definePageMeta({
+        layout: "custom",
+    });
+
+    const {makes} = useToilets();
+</script>
+
+<template>
+    <div>
+        <div class="mt-24">
+            <h1 class="text-6xl">Create a New Listing</h1>
+        </div>
+        <div class="shadow rounded p-3 mt-5 flex flex-wrap justify-between">
+            <ToiletAdSelect title="Make *" :options="makes"></ToiletAdSelect>
+        </div>
+    </div>
+</template>
